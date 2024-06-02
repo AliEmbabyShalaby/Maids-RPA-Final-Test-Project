ad179ef1711fd445dc78989df6f185064ccb50ee
AliEmbabyShalaby <ali.a.shalaby@hotmail.com>
2024-06-02 06:52:42 +0300

    upload

---
+++ UIPATHTest.bat
@@ -0,0 +1,9 @@
+@echo off
+:: Set the path to UiRobot.exe
+set UiRobotPath="C:\Users\aliae\AppData\Local\Programs\UiPath\Studio\UiRobot.exe"
+
+:: Set the path to your .nupkg file
+set PackagePath="C:\Users\aliae\OneDrive\Documents\UiPath\EmailProcessorBotFlow\EmailProcessorBotFlow.1.0.5.nupkg"
+
+:: Run the UiPath process
+%UiRobotPath% execute --file %PackagePath%
